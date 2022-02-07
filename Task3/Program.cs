6cd80dc350232b1598bbef36a046fc1df1d09453
Cherichenko_Sl <Cherry-slava@yandex.ru>
2022-02-07 17:49:42 +0300

    Task3 finished

---
+++ Task3/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.WriteLine("Введите строку");
+string someString =Console.ReadLine();
+char[] reverse = new char[someString.Length];
+for(int i=someString.Length-1; i>=0; i--)
+{
+    reverse[i] = someString[i];
+    Console.Write(reverse[i]);
+}
+
