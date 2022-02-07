69bbc8ca429ec928dd64c360be99b20c1a67d56a
Cherichenko_Sl <Cherry-slava@yandex.ru>
2022-02-07 06:01:04 +0300

    Task1 finished

--- Task1/Program.cs
+++ Task1/Program.cs
@@ -1,2 +1,27 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int[,] matrix = { { 23, 431, 654, 72, 56 }, { 12, 65, 87, 98, 90 }, { 2, 22, 255, 879, 122 }, { 43, 23, 77, 12, 0 }, { 33, 260, 55, 279, 120 } };
+int rows = matrix.GetUpperBound(0) + 1;
+int columns = matrix.Length / rows;
+int j = 0;
+Console.Write("Главная диагональ матрицы: ");
+for (int i = 0; i < rows; i++)
+{
+
+    while (j < columns)
+    {
+        Console.Write($"{matrix[i, j]} ");
+        break;
+    }
+    j++;
+}
+Console.WriteLine();
+Console.Write("Побочная диагональ матрицы: ");
+int a = 0;
+for (int b = columns - 1; b >= 0; b--)
+{
+    while (a < rows)
+    {
+        Console.Write($"{matrix[a, b]} ");
+        break;
+    }
+    a++;
+}
